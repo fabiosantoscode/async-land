3d58623c7d75fe2eab969a1e676719640e16997b
Fábio Santos <fabiosantosart@gmail.com>
2017-10-17 19:52:47 +0100

    feat: chain ==, < and >

--- library.js
+++ library.js
@@ -4,7 +4,25 @@ exports.map = (list, fn) => list.map(fn)
 exports.forEach = (list, fn) => list.forEach(fn)
 exports.identity = () => x => x
 
-const operators = ['<>+-*/|', '=='].reduce((accum, char) => typeof char === 'string' ? accum.concat(char.split('')) : accum.concat(char), [])
+const logicalOps = ['<', '>', '==']
+
+for (const op of logicalOps) {
+  exports[op] = eval(`
+    (...args) => {
+      const STOP = Symbol()
+      return args.reduce((a, b) => {
+        if (a == STOP) {
+          return STOP
+        }
+        if (eval('a ${op} b')) {
+          return b
+        }
+        return STOP
+      }) === STOP ? false : true
+    }`)
+}
+
+const operators = '+-*/|'.split('')
 
 for (const op of operators) {
   exports[op] = eval(`
--- test/library.js
+++ test/library.js
@@ -9,7 +9,8 @@ describe('library', () => {
     ok.deepEqual(lib.map(immutable.List.of(2, 3), (x) => x - 1), immutable.List.of(1, 2))
   })
   it('<', () => {
-    ok.equal(lib['<'](1, 2), true)
+    ok.equal(lib['<'](1, 2, 3), true)
+    ok.equal(lib['<'](1, 3, 2), false)
     ok.equal(lib['<'](2, 1), false)
   })
   it('>', () => {
